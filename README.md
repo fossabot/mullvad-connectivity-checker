2bcb28bee0c4eec8136622572a34e3ef95aa7940
Andreas Basurto <1514352+nitrohorse@users.noreply.github.com>
2018-02-02 00:05:30 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mullvad Connectivity Checker
+
+Browser extension that checks every minute that you are connected to a Mullvad VPN server or not. Powered by [Mullvad](https://am.i.mullvad.net/api).
+
+![alt-text](https://i.imgur.com/XSOgj74.png)
+
+![alt-text](https://i.imgur.com/n2Ij9b5.png)
+
+## Install Locally
+* Firefox - [temporary](https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Temporary_Installation_in_Firefox)
+* Chrome - [permanent](https://superuser.com/questions/247651/how-does-one-install-an-extension-for-chrome-browser-from-the-local-file-system/247654#247654)
+
+## Test Locally
+* Clone the repo
+* Install npm module(s): `yarn` or `npm install`
+* Use [web-ext](https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Getting_started_with_web-ext) for bundling (`yarn bundle` or `npm run bundle`) and development (`yarn web-ext` or `npm run web-ext`)
---
+++ add-on/background.js
@@ -0,0 +1,122 @@
+(function() {
+	'use strict';
+
+	const alert = 'disconnectivityNotification';
+
+	function setBadgeBackgroundColor(color) {
+		chrome.browserAction.setBadgeBackgroundColor({
+			color: color
+		});
+	}
+
+	function setBadgeText(text) {
+		chrome.browserAction.setBadgeText({
+			text: text
+		});
+	}
+
+	function setBadgeTitle(title) {
+		chrome.browserAction.setTitle({
+			title: title
+		});
+	}
+
+	function createActiveTabTo(url) {
+		chrome.tabs.create({
+			active: true,
+			url: url
+		});
+	}
+
+	function setupPriceFetchInterval() {
+		window.setInterval(() => {
+			updateBadge();
+		}, 15000);
+	}
+
+	function updateBadgeAsInfoNotAvailable() {
+		setBadgeBackgroundColor('red');
+		setBadgeText('N/A');
+	}
+
+	function updateBadgeWith(mullvadInfo) {
+		let badgeTitle = '';
+
+		if (mullvadInfo.isConnected && mullvadInfo.city !== null) {
+			badgeTitle = mullvadInfo.ip + ' | ' + mullvadInfo.city + ', ' + mullvadInfo.country + ' | ' + mullvadInfo.server + ' | ' + mullvadInfo.serverType;
+			setBadgeText('✔');
+			setBadgeBackgroundColor('green');
+		} else {
+			badgeTitle = 'Connectivity to Mullvad server lost';
+			setBadgeText('✘');
+			setBadgeBackgroundColor('red');
+
+			chrome.notifications.create(alert, {
+				type: 'basic',
+				message: 'Please check your VPN connection',
+				title: badgeTitle
+			});
+		}
+		setBadgeTitle(badgeTitle);
+	}
+
+	function getInfoFrom(response) {
+		response = JSON.parse(response);
+		return {
+			ip: response['ip'],
+			country: response['country'],
+			city: response['city'],
+			isConnected: response['mullvad_exit_ip'],
+			server: response['mullvad_exit_ip_hostname'],
+			serverType: response['mullvad_server_type'],
+		};
+	}
+
+	function makeRequestForConnectivityInfo() {
+		return new Promise((resolve, reject) => {
+			const xhr = new XMLHttpRequest();
+
+			xhr.open('GET', 'https://am.i.mullvad.net/json', true);
+			xhr.onload = () => {
+				if (xhr.status >= 200 && xhr.status < 300) {
+					resolve(xhr.response);
+				} else {
+					reject({
+						status: xhr.status,
+						statusText: xhr.statusText
+					});
+				}
+			};
+			xhr.onerror = () => {
+				reject({
+					status: xhr.status,
+					statusText: xhr.statusText
+				});
+			};
+			xhr.send();
+		});
+	}
+
+	function updateBadge() {
+		makeRequestForConnectivityInfo().then((response) => {
+			updateBadgeWith(getInfoFrom(response));
+		})
+		.catch(err => {
+			console.error('Error:', err);
+			updateBadgeAsInfoNotAvailable();
+		});
+	}
+
+	function setupBadge() {
+		setBadgeBackgroundColor('#F86922');
+		setBadgeText('...');
+
+		chrome.browserAction.onClicked.addListener((tab) => {
+			createActiveTabTo('https://am.i.mullvad.net');
+		});
+	}
+
+	setupBadge();
+	updateBadge();
+	setupPriceFetchInterval();
+})();
