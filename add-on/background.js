4f82952c9e8b2418c36e9ddc0d4d6d3cb953c000
Andreas Basurto <1514352+nitrohorse@users.noreply.github.com>
2018-02-07 10:58:05 -0800

    [npm] Use Standard rather than ESLint + Prettier
    https://github.com/standard/standard

--- add-on/background.js
+++ add-on/background.js
@@ -1,134 +1,134 @@
-(function() {
-  "use strict";
+(function () {
+  'use strict'
 
-  const alert = "disconnectivityNotification";
-  const updateInterval = 60000; // ms
+  const alert = 'disconnectivityNotification'
+  const updateInterval = 60000 // ms
 
-  const checkConnection = "Please check your VPN connection";
-  const connectivityNA = "Connectivity could not be determined";
-  const connectionLost = "Connectivity to Mullvad server lost";
-  const amIMullvad = "https://am.i.mullvad.net";
-  const amIMullvadJson = amIMullvad + "/json";
+  const checkConnection = 'Please check your VPN connection'
+  const connectivityNA = 'Connectivity could not be determined'
+  const connectionLost = 'Connectivity to Mullvad server lost'
+  const amIMullvad = 'https://am.i.mullvad.net'
+  const amIMullvadJson = amIMullvad + '/json'
 
   const setBadgeBackgroundColorAs = color => {
-    chrome.browserAction.setBadgeBackgroundColor({
+    window.chrome.browserAction.setBadgeBackgroundColor({
       color: color
-    });
-  };
+    })
+  }
 
   const setBadgeTextAs = text => {
-    chrome.browserAction.setBadgeText({
+    window.chrome.browserAction.setBadgeText({
       text: text
-    });
-  };
+    })
+  }
 
   const setBadgeTitleAs = title => {
-    chrome.browserAction.setTitle({
+    window.chrome.browserAction.setTitle({
       title: title
-    });
-  };
+    })
+  }
 
   const createActiveTabTo = url => {
-    chrome.tabs.create({
+    window.chrome.tabs.create({
       active: true,
       url: url
-    });
-  };
+    })
+  }
 
   const setupConnectivityCheckInterval = () => {
     window.setInterval(() => {
-      updateBadge();
-    }, updateInterval);
-  };
+      updateBadge()
+    }, updateInterval)
+  }
 
   const throwNotificationWithBadgeTitleAs = badgeTitle => {
-    chrome.notifications.create(alert, {
-      type: "basic",
+    window.chrome.notifications.create(alert, {
+      type: 'basic',
       message: checkConnection,
       title: badgeTitle
-    });
-  };
+    })
+  }
 
   const updateBadgeAsConnectionInfoNotAvailable = () => {
-    setBadgeBackgroundColorAs("red");
-    setBadgeTextAs("N/A");
-    setBadgeTitleAs(connectivityNA);
-    throwNotificationWithBadgeTitleAs(connectivityNA);
-  };
+    setBadgeBackgroundColorAs('red')
+    setBadgeTextAs('N/A')
+    setBadgeTitleAs(connectivityNA)
+    throwNotificationWithBadgeTitleAs(connectivityNA)
+  }
 
   const isConnectionThroughProxy = connectionType => {
-    return connectionType.includes("SOCKS");
-  };
+    return connectionType.includes('SOCKS')
+  }
 
   const updateBadgeWith = connectionInfo => {
     if (connectionInfo.isConnected) {
-      setBadgeTextAs("✔");
+      setBadgeTextAs('✔')
 
       if (isConnectionThroughProxy(connectionInfo.type)) {
-        setBadgeTextAs("✔✔");
+        setBadgeTextAs('✔✔')
       }
-      setBadgeBackgroundColorAs("green");
+      setBadgeBackgroundColorAs('green')
       const badgeTitle =
         connectionInfo.ip +
-        " | " +
+        ' | ' +
         connectionInfo.city +
         connectionInfo.country +
-        " | " +
+        ' | ' +
         connectionInfo.server +
-        " | " +
-        connectionInfo.type;
-      setBadgeTitleAs(badgeTitle);
+        ' | ' +
+        connectionInfo.type
+      setBadgeTitleAs(badgeTitle)
     } else {
-      setBadgeTextAs("✘");
-      setBadgeBackgroundColorAs("red");
-      throwNotificationWithBadgeTitleAs(connectionLost);
-      setBadgeTitleAs(connectionLost);
+      setBadgeTextAs('✘')
+      setBadgeBackgroundColorAs('red')
+      throwNotificationWithBadgeTitleAs(connectionLost)
+      setBadgeTitleAs(connectionLost)
     }
-  };
+  }
 
   const getConnectionInfoFrom = response => {
-    response = JSON.parse(response);
+    response = JSON.parse(response)
     return {
-      ip: response["ip"],
-      country: response["country"],
-      city: response["city"] ? response["city"] + ", " : "",
-      isConnected: response["mullvad_exit_ip"],
-      server: response["mullvad_exit_ip_hostname"],
-      type: response["mullvad_server_type"]
-    };
-  };
+      ip: response['ip'],
+      country: response['country'],
+      city: response['city'] ? response['city'] + ', ' : '',
+      isConnected: response['mullvad_exit_ip'],
+      server: response['mullvad_exit_ip_hostname'],
+      type: response['mullvad_server_type']
+    }
+  }
 
   const makeRequestForConnectionInfo = () => {
     return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", amIMullvadJson, true);
-      xhr.onload = () => resolve(xhr.response);
-      xhr.onerror = () => reject();
-      xhr.send();
-    });
-  };
+      const xhr = new window.XMLHttpRequest()
+      xhr.open('GET', amIMullvadJson, true)
+      xhr.onload = () => resolve(xhr.response)
+      xhr.onerror = (xhr) => reject(xhr)
+      xhr.send()
+    })
+  }
 
   const updateBadge = () => {
     makeRequestForConnectionInfo()
       .then(response => {
-        updateBadgeWith(getConnectionInfoFrom(response));
+        updateBadgeWith(getConnectionInfoFrom(response))
       })
       .catch(() => {
-        updateBadgeAsConnectionInfoNotAvailable();
-      });
-  };
+        updateBadgeAsConnectionInfoNotAvailable()
+      })
+  }
 
   const setupBadge = () => {
-    setBadgeBackgroundColorAs("#F86922");
-    setBadgeTextAs("...");
-    setBadgeTitleAs(connectivityNA);
+    setBadgeBackgroundColorAs('#F86922')
+    setBadgeTextAs('...')
+    setBadgeTitleAs(connectivityNA)
 
-    chrome.browserAction.onClicked.addListener(() => {
-      createActiveTabTo(amIMullvad);
-    });
-  };
+    window.chrome.browserAction.onClicked.addListener(() => {
+      createActiveTabTo(amIMullvad)
+    })
+  }
 
-  setupBadge();
-  updateBadge();
-  setupConnectivityCheckInterval();
-})();
+  setupBadge()
+  updateBadge()
+  setupConnectivityCheckInterval()
+})()
