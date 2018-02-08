506bb49abdac2f836502da8aed83c1fe9a739c0a
Andreas Basurto <1514352+nitrohorse@users.noreply.github.com>
2018-02-07 21:16:19 -0800

    WIP: Add toggle for WebRTC

--- add-on/popup/popup.js
+++ add-on/popup/popup.js
@@ -1,5 +1,11 @@
 (() => {
+  const toggleElement = document.querySelector('input[name=protectionsToggle]')
+  const toggleTextElement = document.getElementById('toggle-text')
   const amIMullvad = 'https://am.i.mullvad.net'
+  const labelProtectionsOn = 'Protections On'
+  const labelProtectionsOff = 'Protections Off'
+  const webRTCDisabled = 'disable_non_proxied_udp'
+  const webRTCEnabled = 'default'
 
   const openNewTabTo = url => {
     window.chrome.tabs.create({
@@ -24,4 +30,58 @@
     .addEventListener('click', e => {
       openNewTabTo(amIMullvad)
     })
+
+  const enableWebRTC = () => {
+    window.chrome.privacy.network.webRTCIPHandlingPolicy.set({
+      value: webRTCEnabled
+    })
+
+    window.chrome.privacy.network.peerConnectionEnabled.set({
+      value: true
+    })
+  }
+
+  const disableWebRTC = () => {
+    window.chrome.privacy.network.webRTCIPHandlingPolicy.set({
+      value: webRTCDisabled
+    })
+
+    window.chrome.privacy.network.peerConnectionEnabled.set({
+      value: false
+    })
+  }
+
+  const getWebRTCIPHandlingPolicyPromise = () => {
+    return window.browser.privacy.network.webRTCIPHandlingPolicy.get({})
+  }
+
+  const getPeerConnectionEnabledPromise = () => {
+    return window.browser.privacy.network.peerConnectionEnabled.get({})
+  }
+
+  toggleElement.addEventListener('change', () => {
+    if (toggleElement.checked) {
+      disableWebRTC()
+      toggleTextElement.textContent = labelProtectionsOn
+      toggleTextElement.style.backgroundColor = 'transparent'
+    } else {
+      enableWebRTC()
+      toggleTextElement.textContent = labelProtectionsOff
+      toggleTextElement.style.backgroundColor = '#7b2120'
+    }
+  })
+
+  // Check if protections are already on
+  ;(() => {
+    Promise.all([
+      getWebRTCIPHandlingPolicyPromise(),
+      getPeerConnectionEnabledPromise()
+    ]).then(values => {
+      if (values[0].value === webRTCDisabled && values[1].value === false) {
+        toggleElement.checked = true
+        toggleTextElement.textContent = labelProtectionsOn
+        toggleTextElement.style.backgroundColor = 'transparent'
+      }
+    })
+  })()
 })()
